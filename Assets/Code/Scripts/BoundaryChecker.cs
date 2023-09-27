15b3331e4ce386b641778347bfeeade182967243
JackyL56 <haijiejacky@gmail.com>
2023-09-27 12:53:27 +0200

    Add: Popup with animation when player gets close to the boundary.
    A canvas with a popup panel was added, which shows if the player leaves the designated play area. A quick animation was added, where it shows and fades after 3 seconds.

--- Assets/Code/Scripts/BoundaryChecker.cs
+++ Assets/Code/Scripts/BoundaryChecker.cs
@@ -1,17 +1,45 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BoundaryChecker : MonoBehaviour
 {
+
+    [SerializeField] private GameObject boundaryWarningPopup;
+    private Animator _boundaryWarningAnimator;
+    private bool _popupIsActive;
+    private float _animationTime = 3f; // Adjust if animation is changed
     private void Awake()
     {
-        
+        _popupIsActive = false;
+        boundaryWarningPopup.SetActive(false);
+        _boundaryWarningAnimator = boundaryWarningPopup.GetComponent<Animator>();
     }
 
     private void OnTriggerExit(Collider other)
     {
-        Debug.Log(other.name + " exited TriggerArea.");
+        // boundaryWarningPopup.SetActive(true);
+        // _boundaryWarningAnimator.SetTrigger("Show");
+        Debug.Log("Player Exited Area! " +" Active Popup:"+  _popupIsActive);
+        if (_popupIsActive) return;
+        StartCoroutine(ShowPopUp(_animationTime));
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // boundaryWarningPopup.SetActive(false);
+        Debug.Log("Player Entered Area!");
+    }
+
+
+    private IEnumerator ShowPopUp(float waitTime)
+    {
+        Debug.Log("Played");
+        boundaryWarningPopup.SetActive(true);
+        _popupIsActive = true;
+        _boundaryWarningAnimator.SetTrigger("Show");
+        yield return new WaitForSeconds(waitTime);
+        boundaryWarningPopup.SetActive(false);
+        _popupIsActive = false;
     }
 }

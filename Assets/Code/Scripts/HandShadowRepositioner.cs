8d2fc88781e2cc7c2d055e1370c5b2b77e99d54d
JackyL56 <haijiejacky@gmail.com>
2023-11-01 04:16:17 +0100

    Add: Controller Hand Shadows
    Added Hand Shadows for Controller Hands.
    TODO: Disable shadows when switching to hand-tracking!

---
+++ Assets/Code/Scripts/HandShadowRepositioner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class HandShadowRepositioner : MonoBehaviour
+{
+
+    [SerializeField] private float xOffset;
+    [SerializeField] private float scaleX = 1;
+    [SerializeField] private float scaleY = 1;
+    [SerializeField] private float scaleZ = 1;
+    [SerializeField] private Transform handAnchor;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Assert.IsNotNull(handAnchor);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = handAnchor.localPosition;
+        transform.localPosition = new Vector3(xOffset + pos.x * scaleX, pos.y * scaleY, pos.z * scaleZ);
+    }
+}
